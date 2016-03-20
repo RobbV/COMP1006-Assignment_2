d433c106bdc67545bc93471d72fa6308dd94a7fb
RobbV <robbert@robberts-MacBook-Pro-2.local>
2016-03-20 13:39:09 -0400

    1.4

--- save-reg.php
+++ save-reg.php
@@ -5,5 +5,6 @@ require('head.php');
 //generate 10 digit unique user id and store to variable
 $user_id = rand(1000000000,9999999999);
 echo '<h1>'$user_id'</h1>';
+require('footer.php');
 ?>
 // pass the user input to variables
